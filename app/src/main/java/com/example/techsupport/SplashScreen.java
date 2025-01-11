1c786596826cb791f7d204aec57a37a496088932
ConexaMedia <yusufseptiana@gmail.com>
2025-01-11 15:21:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/techsupport/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.techsupport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() ->{
+            Intent intent = new Intent(SplashScreen.this, login.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}

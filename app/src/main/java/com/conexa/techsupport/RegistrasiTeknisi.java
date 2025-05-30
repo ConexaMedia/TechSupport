3bd872a76f3a7bc608df2db134f1cd651ed2d7ce
ConexaMedia <yusufseptiana@gmail.com>
2025-05-31 05:00:58 +0700

    last

--- app/src/main/java/com/conexa/techsupport/RegistrasiTeknisi.java
+++ app/src/main/java/com/conexa/techsupport/RegistrasiTeknisi.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegistrasiTeknisi extends AppCompatActivity {
 
-    private Button btn_backLogin;
+    private TextInputEditText inputNoRegistKaryawan, inputNamaTeknisi, inputEmail, inputPassword;
+    private Button btn_registrasi, btn_backLogin;
+
+    private FirebaseAuth Auth;
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +31,87 @@ public class RegistrasiTeknisi extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrasi_teknisi);
 
+        Auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("teknisi");
+
+        //baca inputan xml
+        inputNoRegistKaryawan = findViewById(R.id.inputNoRegistKaryawan);
+        inputNamaTeknisi = findViewById(R.id.inputNamaTeknisi);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btn_registrasi = findViewById(R.id.btn_registrasi);
         btn_backLogin = findViewById(R.id.btn_backLogin);
 
-        btn_backLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btn_registrasi.setOnClickListener(v -> registerUser());
+        btn_backLogin.setOnClickListener(v ->  {
                 Intent backLogin = new Intent(RegistrasiTeknisi.this, login.class);
                 startActivity(backLogin);
-
-            }
+                finish();
         });
 
+    }
+
+    //ambil dari inputan
+    private void registerUser(){
+        String noRegist = inputNoRegistKaryawan.getText().toString().trim();
+        String namaTeknisi = inputNamaTeknisi.getText().toString().trim();
+        String email = inputEmail.getText().toString().trim();
+        String password = inputPassword.getText().toString();
+
+        //validasi inputan
+        if (namaTeknisi.isEmpty() || noRegist.isEmpty() || email.isEmpty() || password.isEmpty()){
+            if (namaTeknisi.isEmpty()) inputNamaTeknisi.setError("Nama Tidak Boleh Kosong");
+            if (noRegist.isEmpty()) inputNoRegistKaryawan.setError("Harap Isi Nomor Karyawan");
+            if (email.isEmpty()) inputEmail.setError("Email Tidak Boleh Kosong");
+            if (password.length()< 6)inputPassword.setError("Password Minimal 6 Karakter");
+        }
+
+        //simpan data di Realtime Database
+        Auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        //user berhasil dibuat dan disimpan ke database
+                        FirebaseUser firebaseUser = Auth.getCurrentUser();
+                        if(firebaseUser != null){
+
+                            String uid = firebaseUser.getUid();
+                            User teknisi = new User(noRegist, namaTeknisi, email, firebaseUser.getUid());
+
+                            databaseReference.child(noRegist).setValue(teknisi)
+                                    .addOnCompleteListener(saveTask ->{
+                                        if (saveTask.isSuccessful()){
+                                            Toast.makeText(RegistrasiTeknisi.this, "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+
+                                            Intent intent = new Intent(RegistrasiTeknisi.this, login.class);
+                                            startActivity(intent);
+                                            finish();
+                                        } else {
+                                            Toast.makeText(RegistrasiTeknisi.this, "Gagal Simpan Data : " +task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                        }
+                    } else {
+                        Toast.makeText(RegistrasiTeknisi.this, "Registrasi Gagal : "+task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+
+                });
+    }
+
+    public static class User{
+        public String noRegister;
+        public String nama;
+        public String email;
+        public String uid;
+
+        public User(){
+
+        }
+
+        public User(String noRegister, String nama, String email, String uid){
+            this.noRegister = noRegister;
+            this.nama = nama;
+            this.email = email;
+            this.uid = uid;
+        }
     }
 }
--- app/src/main/java/com/conexa/techsupport/login.java
+++ app/src/main/java/com/conexa/techsupport/login.java
@@ -8,14 +8,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class login extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
+    private EditText email, password;
     private Button btn_login;
     private Button btn_registrasi;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,37 +33,63 @@ public class login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.inputUsername);
+        email = findViewById(R.id.inputEmail);
         password = findViewById(R.id.inputPassword);
         btn_login = findViewById(R.id.btn_login);
         btn_registrasi = findViewById(R.id.btn_registrasi);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-                if(validasi(user,pass)){
-                    Intent intent = new Intent(login.this, MainActivity.class);
-                    intent.putExtra(panelHome.NamaTeknisi, user);
-                    intent.putExtra(panelProfile.NamaTeknisi, user);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(login.this, "Username atau Password Salah", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        Auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("teknisi");
+
+        btn_login.setOnClickListener(v -> loginUser());
         btn_registrasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent toRegist = new Intent(login.this, RegistrasiTeknisi.class);
                 startActivity(toRegist);
+                finish();
             }
         });
     }
-    private boolean validasi(String user, String password){
-        return user.equals("PABPROJECT")&& password.equals("123456");
-    }
 
+    private void loginUser() {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+
+        if (user.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(login.this, "Silakan isi username dan password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Auth.signInWithEmailAndPassword(user,pass)
+                .addOnCompleteListener(task ->{
+                    if (task.isSuccessful()){
+                        FirebaseUser firebaseUser = Auth.getCurrentUser();
+                        if(firebaseUser !=null ){
+                            String uid = firebaseUser.getUid();
+
+                            //cari data teknisi berdasarkan email
+                            databaseReference.orderByChild("uid").equalTo(uid)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            if (snapshot.exists()) {
+
+                                                Intent intent = new Intent(login.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            } else {
+                                                Toast.makeText(login.this, "Teknisi belum terdaftar", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+                                        }
+                                    });
+                        }
+                    } else {
+                        Toast.makeText(this, "Email atau Password salah", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
